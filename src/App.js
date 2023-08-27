508513bdd089c13d5bf9e3b96fdca6f361088c26
emon-webdev <emon.webdev@gmail.com>
2023-08-27 23:59:01 +0600

    whatsapp add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import routers from "./Routes/router";
-
+import emon from './images/emon.jpg';
 function App() {
   return (
     <div className="">
       <RouterProvider router={routers} />
       <Toaster position="top-center" reverseOrder={false} />
+
+      <FloatingWhatsApp
+        phoneNumber="01919371381"
+        accountName="Emon Hossain"
+        avatar={emon}
+        allowEsc
+        allowClickAway
+        notification
+        notificationSound
+      />
     </div>
   );
 }
